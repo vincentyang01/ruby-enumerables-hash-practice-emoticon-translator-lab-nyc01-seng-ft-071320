4ea1e5c12e7eebad9f65261360756cfcbf2e1c66
Vincent Yang <vincentyangcsi@gmail.com>
2020-07-06 23:50:46 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,21 @@
-# require modules here
+require "yaml"
+require "pry"
 
-def load_library
-  # code goes here
+path = './lib/emoticons.yml'
+
+def load_library(path)
+  emoticons = YAML.load_file(path)
+  final = emoticons.each do |key, value|
+    #value.each do |engFaces, japFaces|
+    #value returns the array with english and japanese 
+    #value[0] returns english emote, value[1] returns japanese emote
+    #
+    
+    { key => { :english => value[0], :japanese => value[1] }}
+    #binding.pry
+    
+  end
+  binding.pry
 end
 
 def get_japanese_emoticon
